463cbbe99dc599378748fa17222e267fc23484fa
Kritesh Timsina <kriteshtimsina55@gmail.com>
2023-11-18 20:13:34 +0545

    inital setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db";
-import { errorHandler, notFound } from "./middleware/error";
+import { errorHandler, notFound } from "./middlewares/errorMiddleware";
 import bodyParser from "body-parser";
 
 const app = express();
